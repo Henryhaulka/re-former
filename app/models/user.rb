0f304a82059dc81c078249e69fc12aebe6c4c1e8
henryhaulka <onuhenry24@gmail.com>
2021-06-02 11:35:34 +0100

    fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true, format:{ with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, on: :create}
-    validates :password, presence: true, length: { minimum: 7}
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, on: :create }
+  validates :password, presence: true, length: { minimum: 7 }
 end
