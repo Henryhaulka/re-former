ae5a59218ed823ba24ea0c539295c8f62d398f7a
henryhaulka <onuhenry24@gmail.com>
2021-06-02 09:28:15 +0100

    updated views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
     def show
         @user = User.find(params[:id])
     end
+    
+        
     def new
         @user = User.new
     end
+
     def create
         @user = User.new(user_params)
         if @user.save
@@ -12,6 +19,14 @@ class UsersController < ApplicationController
         else
             render :new
         end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+        if @user.edit(user_params)
+            redirect_to @user
+        else
+            render :edit
     end
 
     private
